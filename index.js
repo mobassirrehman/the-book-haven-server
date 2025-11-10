b824bd80fb6fd86cac628958aa116a1f2dfd255a
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2025-11-10 11:29:36 +0600

    project set-up,mongoDB connected, .env add, API test successfull

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0zmmwcn.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Book Haven server is running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    app.get("/test", (req, res) => {
+      res.send("Book Haven API is working!");
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.listen(port, () => {
+  console.log(`Book Haven server is running on port: ${port}`);
+});
